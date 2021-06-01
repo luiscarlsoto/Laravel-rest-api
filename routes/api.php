4fbb0709c9f7e0077b3b15be98fd6d86a2dde7d0
luiscarlsoto <luiscarlsoto@gmail.com>
2021-06-01 13:14:46 -0400

    first commit

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::post('post', function ($id) {
+
+});
+
+Route::get('get', function ($id) {
+
+});
+
+Route::delete('delete', function ($id) {
+
+});
+
+Route::put('update', function ($id) {
+
+});
